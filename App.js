5904f45a95121b8245c8742a27f968e83fe82fd4
Yashraj Raj <rajrishi2329@gmail.com>
2024-05-19 10:53:50 -0400

    swap number

---
+++ App.js
@@ -0,0 +1,14 @@
+//swap the number
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+a = a^b;
+b = a^b;
+c = a^b;
+
+console.log(a,b,c);
+
+//  so this how you can swap the number without using airthmatic oerations in JavaScripts 
+
